dfade767ee71fad00799bfc9c82c4ddfe1232019
Silvia García Sánchez <30872921+Garcia091@users.noreply.github.com>
2021-09-12 21:53:20 -0500

    Axios y validación de datos

--- src/components/TaksApp.jsx
+++ src/components/TaksApp.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import axios from 'axios'
+import { isEmpty } from 'lodash'
+import React, { useState } from 'react'
+import shortid from 'shortid'
 import { useForm } from '../hook/useForm'
 
 const TaksApp = () => {
+    const urlData = 'http://localhost:4000/tareas'
+    const [errorForm, setErrorForm] = useState(false)
 
     const [formValue, handleInputChange, reset] = useForm({
-        url:'',
+        url: '',
         name: '',
         description: ''
     })
+
+    const { url, name, description } = formValue
+
+    const newTask = {
+        id: shortid.generate(),
+        url,
+        name,
+        description,
+    }
+
+    const formValidation = (params) => {
+        if (isEmpty(url) || isEmpty(name) || isEmpty(description)) {
+            setErrorForm(true);
+            return;
+        }
+    }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        formValidation()
+
+        try {
+            const resultado = await axios.post(urlData, newTask)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 
     return (
         <div className="container mt-5">
@@ -27,30 +61,45 @@ const TaksApp = () => {
                 </div>
 
                 <div className="col-4">
-                    <h3 className="text-center">Agregar tareas</h3>
-                    <form className="form-group">
+                    <h3 className="text-center"> Agregar tareas </h3>
+                    {
+                        errorForm &&
+                        <div className="alert alert-danger" role="alert">
+                            Falta información para el envío 
+                        </div>
+
+                    }
+                    <form className="form-group" onSubmit={handleSubmit}>
                         <input
                             type="text"
                             className="form-control "
                             placeholder="Url"
                             name="url"
+                            value={url}
+                            onChange={handleInputChange}
                         />
 
                         <input
                             type="text"
                             className="form-control mt-2"
-                            name="nombre"
+                            name="name"
+                            autoComplete="off"
                             placeholder="Nombre Tarea"
+                            value={name}
+                            onChange={handleInputChange}
                         />
 
                         <textarea
-                            name="sintomas"
+                            name="Description"
                             className="form-control mt-2"
                             autoComplete="off"
+                            name="description"
+                            value={description}
+                            onChange={handleInputChange}
                         >
                         </textarea>
 
-                        <div class="d-grid gap-2 mx-auto mt-2">
+                        <div className="d-grid gap-2 mx-auto mt-2">
                             <button
                                 type="submit"
                                 className="btn btn-dark">
